8e02dc72aa77165d0c7dfe49a22a570b90c2e74b
Robby Stokoe <robbystk@gmail.com>
2019-08-27 11:15:34 -0600

    Add alternative method for getting terminal size

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 /*** includes ***/
 use std::io;
-use std::io::{Read, Write};
+use std::io::{Read, Write, Error, ErrorKind};
 use std::os::unix::io::AsRawFd;
+use std::str;
 
 extern crate termios;
 use termios::*;
@@ -29,11 +30,12 @@ impl EditorConfig {
     /// Includes enabling raw mode and saving the original terminal
     /// configuration for restoration upon exit.
     fn setup() -> EditorConfig {
+        let orig_termios = enable_raw_mode();
         let (rows, cols) = get_window_size()
             .expect("Could not get window size");
 
         EditorConfig {
-            orig_termios: enable_raw_mode(),
+            orig_termios,
             rows,
             cols,
         }
@@ -94,7 +96,29 @@ fn editor_read_key() -> u8 {
     }
 }
 
-fn get_window_size() -> Result<(u16, u16), &'static str> {
+fn get_cursor_position() -> Result<(u16, u16), std::io::Error> {
+    // TODO: rework error handling
+    io::stdout().write(b"\x1b[6n").unwrap();
+    io::stdout().flush().unwrap();
+    let cpr: Vec<u8> = io::stdin().bytes()
+        .fuse()
+        .map(|e| e.unwrap())
+        .collect();
+
+    if cpr[0] != '\x1b' as u8 || cpr[1] != '[' as u8 {
+        return Err(Error::new(ErrorKind::Other,
+            "invalid character position report"));
+    }
+    let data: Vec<u16> = str::from_utf8(&cpr[1..]).unwrap()
+        .trim_matches(|c| c == 'R' || c == '[')
+        .split(';')
+        .map(|s| s.parse().expect("parse error"))
+        .collect();
+
+    Ok((data[0], data[1]))
+}
+
+fn get_window_size() -> Result<(u16, u16), std::io::Error> {
     let mut ws = winsize {
         ws_row: 0,
         ws_col: 0,
@@ -108,7 +132,8 @@ fn get_window_size() -> Result<(u16, u16), &'static str> {
     }
 
     if ret_val == -1 || ws.ws_row == 0 || ws.ws_col == 0 {
-        return Err("ioctl error");
+        io::stdout().write(b"\x1b[999B\x1b[999C").unwrap();
+        return get_cursor_position();
     }
 
     Ok((ws.ws_row, ws.ws_col))
